11196844d9f18e31b1f36ce349eaf3d87a5dd5e3
axel <axel.mastroianni@incloudteam.com>
2023-04-28 14:30:46 +0200

    Till lecture 92

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10.0f;
+    [SerializeField] float jumpSpeed = 5.0f;
+    float originalGravity;
+    CapsuleCollider2D playerCollider;
     Vector2 moveInput;
     Rigidbody2D rb;
     Animator animator;
@@ -13,7 +16,9 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        originalGravity = rb.gravityScale;
         animator = GetComponent<Animator>();
+        playerCollider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
@@ -21,10 +26,20 @@ public class PlayerMovement : MonoBehaviour
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue inputValue) {
         moveInput = inputValue.Get<Vector2>();
+    }
+
+    void OnJump(InputValue inputValue) {
+        if (!playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+            return;
+        }
+        if(inputValue.isPressed) {
+            rb.velocity += new Vector2(0.0f, jumpSpeed);
+        }
     }
 
     void Run() {
@@ -39,6 +54,25 @@ public class PlayerMovement : MonoBehaviour
 
         if (playerHasHorizontalSpeed) {
             transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1.0f);
+        }
+    }
+
+    void ClimbLadder() {
+        if(playerCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
+            Vector2 playerVelocity = new Vector2(rb.velocity.x, moveInput.y * runSpeed);
+            rb.velocity = playerVelocity;
+            rb.gravityScale = 0.0f;
+            bool playerHasVerticalSpeed = Mathf.Abs(rb.velocity.y) > Mathf.Epsilon;
+            if (playerHasVerticalSpeed) {
+                animator.SetBool("IsClimbing", true);
+            }
+            else {
+                animator.SetBool("IsClimbing", false);
+            }
+        }
+        else {
+            rb.gravityScale = originalGravity;
+            animator.SetBool("IsClimbing", false);
         }
     }
 }
